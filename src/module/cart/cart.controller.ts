e00aa75fa048e9f53127dfc431c2f2bf36bc5338
Arbaj Shaikh <shaikharbaj2001@gmail.com>
2024-05-23 17:44:22 +0530

    change

--- src/module/cart/cart.controller.ts
+++ src/module/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Patch, Post, Res, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../user/guards/jwt.guards';
 import { Auth } from '../user/dto/authdto';
 import { Response } from 'express';
@@ -9,6 +9,11 @@ export class CartController {
 
      constructor(private readonly cartservice:CartService){}
 
+    @UseGuards(JwtGuard)
+    @Get("/all")
+    async loadCartProduct(@Auth() auth:any,@Res() res:Response){
+           return await this.cartservice.loadcartproduct(auth,res);
+    }
     @UseGuards(JwtGuard)
     @Post("/add_to_cart")
     async AddToCart(@Auth() auth:any,@Body() data:any,@Res() res:Response){
@@ -23,6 +28,11 @@ export class CartController {
     @Patch("/decrement_quantity/:id")
     async decrementCartProductQuantity(@Auth() auth:any,@Param("id") id:number,@Res() res:Response){
            return await this.cartservice.decrementQuantity(auth,id,res);
+    }
+    @UseGuards(JwtGuard)
+    @Delete("/removeproduct/:id")
+    async removeproductfromcart(@Auth() auth:any,@Param("id") id:number,@Res() res:Response){
+           return await this.cartservice.removeproductfromcart(auth,id,res);
     }
 
 
--- src/module/cart/cart.service.ts
+++ src/module/cart/cart.service.ts
@@ -3,140 +3,212 @@ import { PrismaService } from '../prisma/prismaservice';
 import { Response } from 'express';
 @Injectable()
 export class CartService {
+  constructor(private readonly prisma: PrismaService) {}
 
-    constructor(private readonly prisma: PrismaService) { }
-
-    async add_to_cart(auth: any, data: any, res: Response) {
-        try {
-            const cart = await this.prisma.cart.findFirst({
-                where: {
-                    userId: Number(auth.userId)
-                }
-            })
-            const cartId = cart?.id;
-
-            //check product is already in cart or not if 
-            const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
-                where: {
-                    AND: [
-                        { cartId: cartId },
-                        { productId: Number(data.product_id) }
-                    ]
-                }
-            })
-            let product;
-            if (!checkproductalreadyexist) {
-                product = await this.prisma.cartItem.create({
-                    data: {
-                        cartId: cartId,
-                        productId: Number(data.product_id),
-                        quantity: 1
-                    }
-                })
-            } else {
-                product = await this.prisma.cartItem.update({
-                    where: {
-                        id: Number(checkproductalreadyexist.id)
-                    },
-                    data: {
-                        quantity: { increment: 1 }
-                    }
-                })
-            }
-            // //add product to cart................
-
-            // const product = await this.prisma.cartItem.create({
-            //     data: {
-            //         cartId: cartId,
-            //         productId: Number(data.product_id),
-            //         quantity: Number(data.quantity)
-            //     }
-            // })
-
-            return res.status(HttpStatus.OK).json({ success: true, message: "item added in cart successfully", data: product });
-
-        } catch (error) {
-            return res.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
-        }
+  async loadcartproduct(auth: any, res: Response) {
+    try {
+      const cart = await this.prisma.cart.findFirst({
+        where: {
+          userId: Number(auth.userId),
+        },
+      });
+      //get all product from cart...
+      const cartItem = await this.prisma.cartItem.findMany({
+        include: {
+          product: true,
+        },
+        where: {
+          cartId: cart.id,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      });
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        message: 'cart item fetch successfully',
+        data: cartItem,
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ success: false, message: error.message });
     }
+  }
+  
+  async add_to_cart(auth: any, data: any, res: Response) {
+    try {
+      const cart = await this.prisma.cart.findFirst({
+        where: {
+          userId: Number(auth.userId),
+        },
+      });
+      const cartId = cart?.id;
 
-    async incrementQuantity(auth: any, id: number, res: Response) {
-        try {
-            const cart = await this.prisma.cart.findFirst({
-                where: {
-                    userId: Number(auth.userId)
-                }
-            })
-            const cartId = cart?.id;
+      //check product is already in cart or not if
+      const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
+        where: {
+          AND: [{ cartId: cartId }, { productId: Number(data.product_id) }],
+        },
+      });
+      let product;
+      if (!checkproductalreadyexist) {
+        product = await this.prisma.cartItem.create({
+          include: {
+            product: true,
+          },
+          data: {
+            cartId: cartId,
+            productId: Number(data.product_id),
+            quantity: 1,
+          },
+        });
+      } else {
+        product = await this.prisma.cartItem.update({
+          include: {
+            product: true,
+          },
+          where: {
+            id: Number(checkproductalreadyexist.id),
+          },
+          data: {
+            quantity: { increment: 1 },
+          },
+        });
+      }
 
-            //check product is already in cart or not if 
-            const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
-                where: {
-                    AND: [
-                        { cartId: cartId },
-                        { productId: Number(id) }
-                    ]
-                }
-            })
-            if (!checkproductalreadyexist) {
-                throw new Error("product with this id is not present in this users cart")
-            }
-            //update the quantity of the product......
-            const updatequantity = await this.prisma.cartItem.update({
-                where: {
-                    id: checkproductalreadyexist.id
-                },
-                data: { quantity: { increment: 1 } }
-            })
-
-            return res.status(HttpStatus.OK).json({ message: "quantity incremented successfully", data: updatequantity })
-        } catch (error) {
-            return res.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
-        }
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        message: 'item added in cart successfully',
+        data: product,
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ success: false, message: error.message });
     }
+  }
 
+  async incrementQuantity(auth: any, id: number, res: Response) {
+    try {
+      const cart = await this.prisma.cart.findFirst({
+        where: {
+          userId: Number(auth.userId),
+        },
+      });
+      const cartId = cart?.id;
 
-    async decrementQuantity(auth: any, id: number, res: Response) {
-        try {
-            const cart = await this.prisma.cart.findFirst({
-                where: {
-                    userId: Number(auth.userId)
-                }
-            })
-            const cartId = cart?.id;
+      //check product is already in cart or not if
+      const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
+        where: {
+          AND: [{ cartId: cartId }, { productId: Number(id) }],
+        },
+      });
+      if (!checkproductalreadyexist) {
+        throw new Error(
+          'product with this id is not present in this users cart',
+        );
+      }
+      //update the quantity of the product......
+      const updatequantity = await this.prisma.cartItem.update({
+        where: {
+          id: checkproductalreadyexist.id,
+        },
+        data: { quantity: { increment: 1 } },
+      });
 
-            //check product is already in cart or not if 
-            const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
-                where: {
-                    AND: [
-                        { cartId: cartId },
-                        { productId: Number(id) }
-                    ]
-                }
-            })
-            if (!checkproductalreadyexist) {
-                throw new Error("product with this id is not present in this users cart")
-            }
-
-            if (checkproductalreadyexist.quantity > 1) {
-                const updatequantity = await this.prisma.cartItem.update({
-                    where: {
-                        id: checkproductalreadyexist.id
-                    },
-                    data: { quantity: { decrement: 1 } }
-                })
-            } else {
-                const deleteproduct = await this.prisma.cartItem.delete({
-                    where: {
-                        id: Number(checkproductalreadyexist.id)
-                    }
-                })
-            }
-            //update the quantity of the product.....
-            return res.status(HttpStatus.OK).json({ message: "quantity incremented successfully" })
-        } catch (error) {
-            return res.status(HttpStatus.BAD_REQUEST).json({ success: false, message: error.message });
-        }
+      return res.status(HttpStatus.OK).json({
+        message: 'quantity incremented successfully',
+        data: updatequantity,
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ success: false, message: error.message });
     }
+  }
 
+  async decrementQuantity(auth: any, id: number, res: Response) {
+    try {
+      const cart = await this.prisma.cart.findFirst({
+        where: {
+          userId: Number(auth.userId),
+        },
+      });
+      const cartId = cart?.id;
+
+      //check product is already in cart or not if
+      const checkproductalreadyexist = await this.prisma.cartItem.findFirst({
+        where: {
+          AND: [{ cartId: cartId }, { productId: Number(id) }],
+        },
+      });
+
+      if (!checkproductalreadyexist) {
+        throw new Error(
+          'product with this id is not present in this users cart',
+        );
+      }
+
+      //   if (checkproductalreadyexist.quantity > 1) {
+      let updatedproduct = await this.prisma.cartItem.update({
+        where: {
+          id: checkproductalreadyexist.id,
+        },
+        data: { quantity: { decrement: 1 } },
+      });
+      //   } else {
+      if (updatedproduct.quantity == 0) {
+        await this.prisma.cartItem.delete({
+          where: {
+            id: Number(checkproductalreadyexist.id),
+          },
+        });
+      }
+
+      //update the quantity of the product........
+      return res.status(HttpStatus.OK).json({
+        message: 'quantity decrement successfully.',
+        data: updatedproduct,
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ success: false, message: error.message });
+    }
+  }
+
+  async removeproductfromcart(auth: any, id: number, res: Response) {
+    try {
+      //find cart...
+      const cart = await this.prisma.cart.findFirst({
+        where: {
+          userId: Number(auth.userId),
+        },
+      });
+
+      //find product.....
+      const product = await this.prisma.cartItem.findFirst({
+        where: {
+          AND: [{ cartId: cart.id }, { productId: Number(id) }],
+        },
+      });
+      //delete the product from cartItem...
+      const removeItem = await this.prisma.cartItem.delete({
+        where: {
+          id: Number(product.id),
+        },
+      });
+
+      return res.status(HttpStatus.OK).json({
+        success: true,
+        data: removeItem,
+        message: 'cart item deleted successfully...!',
+      });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ success: false, message: error.message });
+    }
+  }
 }
